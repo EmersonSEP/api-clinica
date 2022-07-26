dfd874c3a1503dc5c6d187acc09435f92921826b
Antonio Emerson <emerson.nessa@gmail.com>
2022-07-26 20:53:40 -0300

    ajuste do dotenv

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-
-//Requires: 
+//Requires:
 
 const express = require("express");
 const db = require("./database");
@@ -7,7 +6,8 @@ const handleError = require("./middlewares/handleError");
 const authMiddleware = require("./middlewares/auth");
 const jwtMiddleware = require("./middlewares/jwt");
 const routes = require("./routes");
-const cors = require ('cors');
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 //Instances:
 
@@ -16,26 +16,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //app.use(jwtMiddleware.unless({ path: [
-  //{ url: "/login", methods: ['POST'] },
-  //{ url: "/psicologos", methods: ['POST'] }
+//{ url: "/login", methods: ['POST'] },
+//{ url: "/psicologos", methods: ['POST'] }
 //] }));
 //app.use(authMiddleware);
-
 
 db.hasConnection();
 
 //Tcp Connection Port:
-
+dotenv.config();
 // const port = 4000;
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-//Uses: 
-app.use (cors());
+//Uses:
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(handleError);
-
 
 app.use((req, res) => {
   res.status(404).json({ message: "URL não encontrada." });
@@ -46,3 +44,4 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log(`[OK] Servidor conectado!!! [Porta TCP: ${port}]`);
 });
+
